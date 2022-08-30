009e21dbe76e268eefa4f81244b66c7c647612e1
Cristhian González <cristhian.gonzalez@udea.edu.co>
2022-08-30 00:38:42 -0500

    feat/Adding UserTypeResolvers to GlobalResolvers

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,7 +1,9 @@
 import { Resolver } from 'types';
 import { model1Resolvers } from '@graphql/server/model1/resolvers';
+import { UserTypeResolvers } from '@graphql/server/userType/resolvers';
 
 const GlobalResolvers: Resolver[] = [
+  UserTypeResolvers,
   model1Resolvers,
   model1Resolvers,
   model1Resolvers,
