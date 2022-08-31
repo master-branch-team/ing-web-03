4335b3c6525f0aa5ed416104d7b9b0513d13bfad
Cristhian González <cristhian.gonzalez@udea.edu.co>
2022-08-31 01:41:44 -0500

    feat/Created some User resolvers and add to GlobalResolvers

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,7 +1,9 @@
 import { Resolver } from 'types';
 import { model1Resolvers } from '@graphql/server/model1/resolvers';
+import { UserResolvers } from '@graphql/server/user/resolvers';
 
 const GlobalResolvers: Resolver[] = [
+  UserResolvers,
   model1Resolvers,
   model1Resolvers,
   model1Resolvers,
--- graphql/server/user/resolvers.ts
+++ graphql/server/user/resolvers.ts
@@ -1,8 +1,50 @@
+import prisma from '@config/prisma';
 import { Resolver } from 'types';
 
 const UserResolvers: Resolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    getUser: async (parent, args) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: args.id,
+        },
+      });
+      return user;
+    },
+    getAllUsers: async (parent, args) => {
+      const users = await prisma.user.findMany();
+      return users;
+    },
+  },
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await prisma.user.create({
+        // data: {
+        //   ...args.data,
+        // },
+        data: {
+          email: args.data.email,
+          full_name: args.data.full_name,
+          phone: args.data.phone,
+          address: args.data.address,
+          photo_link: args.data.photo_link,
+          position: {
+            connect: {
+              id: args.data.position_id,
+            },
+          },
+          user_type: {
+            connect: {
+              id: args.data.user_type_id,
+            },
+          },
+        },
+      });
+      return user;
+    },
+    updateUser: async (parent, args) => {},
+    deleteUser: async (parent, args) => {},
+  },
 };
 
 export { UserResolvers };
