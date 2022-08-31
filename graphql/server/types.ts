33d332ef0168f1d1f9f506da5d713e509a2a1238
Cristhian González <cristhian.gonzalez@udea.edu.co>
2022-08-31 01:34:54 -0500

    feat/Created User types and add to GlobalTypes

--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -3,6 +3,7 @@ import { gql } from 'apollo-server-micro';
 import { CourseTypes } from '@graphql/server/course/types';
 import { model1Types } from '@graphql/server/model1/types';
 import { TrainingTypes } from '@graphql/server/training/types';
+import { UserTypes } from '@graphql/server/user/types';
 
 const CommonTypes = gql`
   scalar Date
@@ -12,6 +13,7 @@ const GlobalTypes: DocumentNode[] = [
   CommonTypes,
   CourseTypes,
   TrainingTypes,
+  UserTypes,
   model1Types,
   model1Types,
   model1Types,
--- graphql/server/user/types.ts
+++ graphql/server/user/types.ts
@@ -17,12 +17,28 @@ const UserTypes = gql`
     trainings: [Training]
   }
 
+  input UserCreateInput {
+    email: String!
+    full_name: String!
+    phone: String!
+    address: String!
+    photo_link: String!
+    position_id: String #¿El campo debería ser 'position' o 'position_id'?
+    user_type_id: String! #¿El campo debería ser 'user_type' o 'user_type_id'?
+  }
+
+  input UserUpdateInput {
+    #Falta por definir
+  }
+
   type Query {
     getUser(id: String): User
     getAllUsers: [User]
   }
   type Mutation {
-    
+    createUser(data: UserCreateInput): User
+    updateUser(id: String, data: UserUpdateInput): User
+    deleteUser(id: String): User
   }
 `;
 
