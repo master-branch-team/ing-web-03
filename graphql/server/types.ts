96d051842a41194fc1ca500d9f11eb8bff16cfa7
Cristhian González <cristhian.gonzalez@udea.edu.co>
2022-09-02 01:32:49 -0500

    fix/GlobalTypes imports

--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -1,14 +1,14 @@
 import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-micro';
-import { CourseTypes } from '@graphql/server/course/types';
-import { TrainingTypes } from '@graphql/server/training/types';
-import { NoteTypes } from '@graphql/server/note/types';
-import { CommentTypes } from '@graphql/server/comment/types';
-import { LikeTypes } from '@graphql/server/like/types';
-import { PositionTypes } from '@graphql/server/position/types';
-import { CourseStateTypes } from '@graphql/server/course-state/types';
-import { UserTypeTypes } from '@graphql/server/userType/types';
 import { UserTypes } from '@graphql/server/user/types';
+import { PositionTypes } from '@graphql/server/position/types';
+import { UserTypeTypes } from '@graphql/server/userType/types';
+import { LikeTypes } from '@graphql/server/like/types';
+import { NoteTypes } from '@graphql/server/note/types';
+import { CourseTypes } from '@graphql/server/course/types';
+import { CourseStateTypes } from '@graphql/server/course-state/types';
+import { TrainingTypes } from '@graphql/server/training/types';
+import { CommentTypes } from '@graphql/server/comment/types';
 
 const CommonTypes = gql`
   scalar Date
@@ -16,15 +16,15 @@ const CommonTypes = gql`
 
 const GlobalTypes: DocumentNode[] = [
   CommonTypes,
-  CourseTypes,
-  TrainingTypes,
-  NoteTypes,
-  CommentTypes
-  LikeTypes,
-  PositionTypes,
-  CourseStateTypes,
-  UserTypeTypes,
   UserTypes,
+  PositionTypes,
+  UserTypeTypes,
+  LikeTypes,
+  NoteTypes,
+  CourseTypes,
+  CourseStateTypes,
+  TrainingTypes,
+  CommentTypes,
 ];
 
 export { GlobalTypes };
