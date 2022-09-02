1c036ddeceef7deb77365a9a7ede609391f2a8a6
natogourmet <natogourmet@hotmail.com>
2022-09-01 20:07:23 -0500

    fix: Changed Comment Type Definition

--- graphql/server/comment/types.ts
+++ graphql/server/comment/types.ts
@@ -1,25 +1,34 @@
-import {gql} from 'apollo-server-micro';
+import { gql } from 'apollo-server-micro';
 
 const CommentTypes = gql`
-    type Comment {
-        id: ID
-        text: String
-    }
-    input CommentCreateInput {
-        text: String
-    }
-    input CommentUpdateInput {
-        text: String
-    }
+  type Comment {
+    id: ID
+    text: String
+    user: User
+    training: Training
+  }
 
-    type Query {
-        getComment(id: string): Comment
-        getComments: [Comment]
-    }
-    type Mutation {
-        createComment(data: CommentCreateInput): Comment
-        updateComment(id: string, data: CommentUpdateInput): Comment
-        deleteComment(id: string): Comment
+  input CommentCreateInput {
+    text: String!
+    user_id: String!
+    training_id: String!
+  }
 
+  input CommentUpdateInput {
+    text: String
+    user_id: String
+    training_id: String
+  }
+
+  type Query {
+    getComment(id: String): Comment
+    getComments: [Comment]
+  }
+
+  type Mutation {
+    createComment(data: CommentCreateInput): Comment
+    updateComment(id: String, data: CommentUpdateInput): Comment
+    deleteComment(id: String): Comment
+  }
 `;
-export {CommentTypes};
+export { CommentTypes };
