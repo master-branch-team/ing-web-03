49591754d060391a01b946982d12cd70dc41a572
natogourmet <natogourmet@hotmail.com>
2022-09-01 20:30:20 -0500

    fiix: removed Example Model

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,7 +1,6 @@
 import { Resolver } from 'types';
-import { model1Resolvers } from '@graphql/server/model1/resolvers';
 import { TrainingResolvers } from '@graphql/server/training/resolvers';
 
-const GlobalResolvers: Resolver[] = [model1Resolvers, TrainingResolvers];
+const GlobalResolvers: Resolver[] = [TrainingResolvers];
 
 export { GlobalResolvers };
--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -1,7 +1,6 @@
 import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-micro';
 import { CourseTypes } from '@graphql/server/course/types';
-import { model1Types } from '@graphql/server/model1/types';
 import { TrainingTypes } from '@graphql/server/training/types';
 import { NoteTypes } from '@graphql/server/note/types';
 import { CommentTypes } from '@graphql/server/comment/types';
@@ -17,8 +16,6 @@ const GlobalTypes: DocumentNode[] = [
   CourseTypes,
   TrainingTypes,
   LikeTypes,
-  model1Types,
-  model1Types,
   NoteTypes,
   PositionTypes,
   CommentTypes,
