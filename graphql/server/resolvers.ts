78a78248316f7baa663bf156da707e8914eb96cd
Ian Marcos <ian.marcos.ortiz@gmail.com>
2022-09-02 21:10:39 -0500

    feat/Created Course resolvers for notes, trainings, course_states, and CRUD operations

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,9 +1,11 @@
 import { Resolver } from 'types';
+import { CourseResolvers } from '@graphql/server/course/resolvers';
 import { TrainingResolvers } from '@graphql/server/training/resolvers';
 import { UserTypeResolvers } from '@graphql/server/userType/resolvers';
 import { UserResolvers } from '@graphql/server/user/resolvers';
 
 const GlobalResolvers: Resolver[] = [
+  CourseResolvers,
   TrainingResolvers,
   UserTypeResolvers,
   UserResolvers,
