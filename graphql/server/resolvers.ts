c6655bd3a35f249e44819fb4842684c09bed2473
natogourmet <natogourmet@hotmail.com>
2022-09-04 20:57:05 -0500

    hotfix: sorted ascending and courseState name

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,24 +1,24 @@
 import { Resolver } from 'types';
-import { CourseResolvers } from '@graphql/server/course/resolvers';
-import { TrainingResolvers } from '@graphql/server/training/resolvers';
-import { UserTypeResolvers } from '@graphql/server/userType/resolvers';
-import { UserResolvers } from '@graphql/server/user/resolvers';
-import { NoteResolvers } from '@graphql/server/note/resolvers';
 import { CommentResolvers } from '@graphql/server/comment/resolvers';
+import { CourseResolvers } from '@graphql/server/course/resolvers';
+import { CourseStateResolvers } from '@graphql/server/courseState/resolvers';
 import { LikeResolvers } from '@graphql/server/like/resolvers';
-import { CourseStateResolvers } from '@graphql/server/course-state/resolvers';
+import { NoteResolvers } from '@graphql/server/note/resolvers';
 import { PositionResolvers } from '@graphql/server/position/resolvers';
+import { TrainingResolvers } from '@graphql/server/training/resolvers';
+import { UserResolvers } from '@graphql/server/user/resolvers';
+import { UserTypeResolvers } from '@graphql/server/userType/resolvers';
 
 const GlobalResolvers: Resolver[] = [
-  CourseResolvers,
-  TrainingResolvers,
-  UserTypeResolvers,
-  UserResolvers,
-  NoteResolvers,
   CommentResolvers,
-  LikeResolvers,
+  CourseResolvers,
   CourseStateResolvers,
+  LikeResolvers,
+  NoteResolvers,
   PositionResolvers,
+  TrainingResolvers,
+  UserResolvers,
+  UserTypeResolvers,
 ];
 
 export { GlobalResolvers };
--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -1,30 +1,30 @@
 import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-micro';
-import { UserTypes } from '@graphql/server/user/types';
-import { PositionTypes } from '@graphql/server/position/types';
-import { UserTypeTypes } from '@graphql/server/userType/types';
+import { CommentTypes } from '@graphql/server/comment/types';
+import { CourseStateTypes } from '@graphql/server/courseState/types';
+import { CourseTypes } from '@graphql/server/course/types';
 import { LikeTypes } from '@graphql/server/like/types';
 import { NoteTypes } from '@graphql/server/note/types';
-import { CourseTypes } from '@graphql/server/course/types';
-import { CourseStateTypes } from '@graphql/server/course-state/types';
+import { PositionTypes } from '@graphql/server/position/types';
 import { TrainingTypes } from '@graphql/server/training/types';
-import { CommentTypes } from '@graphql/server/comment/types';
+import { UserTypes } from '@graphql/server/user/types';
+import { UserTypeTypes } from '@graphql/server/userType/types';
 
 const CommonTypes = gql`
   scalar Date
 `;
 
 const GlobalTypes: DocumentNode[] = [
+  CommentTypes,
   CommonTypes,
-  UserTypes,
-  PositionTypes,
-  UserTypeTypes,
+  CourseStateTypes,
+  CourseTypes,
   LikeTypes,
   NoteTypes,
-  CourseTypes,
-  CourseStateTypes,
+  PositionTypes,
   TrainingTypes,
-  CommentTypes,
+  UserTypes,
+  UserTypeTypes,
 ];
 
 export { GlobalTypes };
